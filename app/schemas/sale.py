94cd90cb6f482175d2fbc3e36099912862a65b68
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-05-12 22:32:26 -0300

    feat: sale schemas and model

---
+++ app/schemas/sale.py
@@ -0,0 +1,75 @@
+from pydantic import Field
+
+from app.schemas import ProductResponse, UserResponse
+from app.schemas.settings.base import BaseSchema
+
+
+class SaleBase(BaseSchema):
+    """
+    Base schema for sale.
+
+    Attributes:
+        product_id (str): The ID of the product.
+        user_id (str): The ID of the user.
+        quantity (float): The quantity of the product sold.
+    """
+
+    product_id: str = Field(examples=["1234567890"])
+    user_id: str = Field(examples=["0987654321"])
+    quantity: float = Field(examples=[1.0])
+
+
+class SaleRequest(SaleBase):
+    """
+    Request schema for sale.
+
+    Attributes:
+        product_id (str): The ID of the product.
+        user_id (str): The ID of the user.
+        quantity (float): The quantity of the product sold.
+    """
+
+    pass
+
+
+class SaleResponse(SaleBase):
+    """
+    Response schema for sale.
+
+    Attributes:
+        product_id (str): The ID of the product.
+        user_id (str): The ID of the user.
+        quantity (float): The quantity of the product sold.
+    """
+
+    id: str = Field(examples=["1234567890"])
+    is_paid: bool = Field(examples=[True])
+    value: float = Field(examples=[10.0])
+    created_at: str = Field(examples=["2023-10-01T12:00:00Z"])
+
+
+class SaleNoteRequest(BaseSchema):
+    """
+    Request schema for sale note.
+
+    Attributes:
+        sales (list[SaleRequest]): A list of sales.
+    """
+
+    sales: list[SaleRequest]
+
+
+class SaleNoteResponse(BaseSchema):
+    """
+    Response schema for sale note.
+    Attributes:
+        client (UserResponse): The user who made the sale.
+        products (list[ProductResponse]): A list of products sold.
+        notes (list[SaleResponse]): A list of sales.
+        value (float): The total value of the sale.
+    """
+
+    client: UserResponse
+    products: list[ProductResponse]
+    notes: list[SaleResponse]
+    value: float
